826a7c9def145de224f40eb15715bc3aa2f753ed
Anh To Thi Van <vananhto.bg@gmail.com>
2019-06-18 17:05:03 +0700

    Create my_config_downloadSRS.py

---
+++ my_config_downloadSRS.py
@@ -0,0 +1,9 @@
+srs_list_file = r'D:\...\srs_id_list.xlsx'
+sheet_name_srs = 'Sheet1'
+
+raw_path_to_save = r'D:\...\SRS_Dowloaded'
+
+firefox_geckodriver = r'D:\...\Wed_driver\geckodriver.exe'
+
+username = 'username'
+password = 'password'
